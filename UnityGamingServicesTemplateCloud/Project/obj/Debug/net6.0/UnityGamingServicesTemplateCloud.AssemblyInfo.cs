bcb75a0164fa2a4ef8ebb6d003371c788dd00281
DevCol <devcol.main@gmail.com>
2026-01-10 13:30:10 +0900

    Add PlayerStats GameObject to PlayerLogin scene
    Introduced a new PlayerStats GameObject with associated MonoBehaviour to the PlayerLogin.unity scene, enabling display and management of player gold and health potion stats. Updated scene hierarchy to include PlayerStats under the main canvas. No functional code changes outside of scene asset updates.

--- UnityGamingServicesTemplateCloud/Project/obj/Debug/net6.0/UnityGamingServicesTemplateCloud.AssemblyInfo.cs
+++ UnityGamingServicesTemplateCloud/Project/obj/Debug/net6.0/UnityGamingServicesTemplateCloud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnityGamingServicesTemplateCloud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03338eca9ca48acdd4c6b923ff1d6268012dce8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed974e7beabd8fb5c6103b6bc59d4dc4ff0b1b34")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnityGamingServicesTemplateCloud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnityGamingServicesTemplateCloud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
